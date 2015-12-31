8bc9856560037430d73bdb814db25ef6c28257a1
ufoufo <1243786870@qq.com>
2015-12-31 09:56:20 +0800

    heillo->测试提交文件

---
+++ hello/hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hello
+//
+//  Created by 牛仔裤 on 15-12-31.
+//  Copyright (c) 2015年 MS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hello/hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hello
+//
+//  Created by 牛仔裤 on 15-12-31.
+//  Copyright (c) 2015年 MS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
